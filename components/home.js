488b0af9cdf6f511e4346a986ace3a6984d8e1ad
MrS-E <simeon.stix@gmx.ch>
2023-02-25 09:42:36 +0100

    add todo notes

--- components/home.js
+++ components/home.js
@@ -3,7 +3,7 @@ const object = require('../src/models/object');
 exports.view = function (req, res, next){
  if(req.session.loggedin) {
 
-  object.count()
+  object.count() //TODO select books with keywords that the user read in the history and don't show the user books he already knows
       .then((count)=>{
        const skip = Math.floor(Math.random() * (count-4));
        object.find().limit(4).skip(skip<0?0:skip)
