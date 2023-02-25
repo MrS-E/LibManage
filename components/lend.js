c2063d15b78f5720a5382c9f83690ab563b121ef
MrS-E <simeon.stix@gmx.ch>
2023-02-25 12:21:38 +0100

    build renting system

--- components/lend.js
+++ components/lend.js
@@ -1,13 +1,43 @@
+const user = require('../src/models/user')
+const object = require('../src/models/object')
+
 exports.view = function(req, res, next){
     if(req.session.loggedin) {
-        res.render('sites/lend', {user: req.session.username, active: 'liste'})
+        object.findOne({_id: req.params.id})
+            .then((doc)=>{
+                if(doc.typ===('E-Book' || 'E-Audio' || 'E-Video')){
+                    res.render('sites/lend', {user: req.session.username, name: req.session.name, active: 'liste', book: doc, can_be_rented: true})
+                }else{
+                    if(typeof(doc.history[doc.history.length-1].end) === Date || doc.history.length === 0){
+                        res.render('sites/lend', {user: req.session.username, name: req.session.name, active: 'liste', book: doc, can_be_rented: true})
+                    }else{
+                        res.render('sites/lend', {user: req.session.username, name: req.session.name, active: 'liste', book: doc, can_be_rented: false})
+                    }
+                }
+            })
+            .catch((err)=>{
+                console.log(err)
+                res.redirect('/liste/'+req.params.id)
+            })
     }
     else{
         res.redirect('/login')
     }
 }
-exports.lend = function (req, res, next){
-
+exports.lend = async function (req, res, next){
+    console.log(req.session.userid, ' ', req.params.id)
+        const d = new Date()
+    await user.updateOne({_id: req.session.userid}, {$push: {history: {book: req.params.id, start: d, end: null}}})
+        .catch(err=>{
+            console.error(err)
+            res.redirect('/liste')
+        })
+    await object.updateOne({_id: req.params.id}, {$push: {history: {user: req.session.userid, start: d, end: null}}})
+        .catch(err=>{
+            console.error(err)
+            res.redirect('/liste')
+        })
+    res.redirect("/home")
 }
 exports.return = function (req, res, next){
 
