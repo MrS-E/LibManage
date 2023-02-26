304381aeb507fe3792d0bb7d727b51525c37c59f
MrS-E <simeon.stix@gmx.ch>
2023-02-26 15:40:53 +0100

    comments to index.js

--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,17 @@ router.get('/home', home.view);
 router.get('/login', login.view);
 router.post('/logout', login.end);
 router.get('/register', register.view);
+
+/*GET liste page*/
 router.get('/liste', liste.view);
 router.get('/liste/:id', liste.object);
+
+/*GET ich page*/
 router.get('/ich', ich.view);
 router.get('/ich/settings', ich.settings);
 router.get('/ich/history', ich.history);
+
+/*GET special pages*/
 router.get('/returns')
 router.get('/admin')
 module.exports = router;
