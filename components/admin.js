1b583a8fc25ce90e7d2a6de3e7c00bbaabb34ad3
MrS-E <simeon.stix@gmx.ch>
2023-03-07 22:18:53 +0100

    adding functionality of adding an object

--- components/admin.js
+++ components/admin.js
@@ -40,6 +40,34 @@ exports.new = function (req, res, next){
 exports.add = function (req, res, next){
     if(req.session.loggedin && req.session.role==='admin'){
         console.log(req.body)
+            objects.count()
+            .then((count)=> {
+                let object = new objects({
+                    _id: (count+1), //fixme selbes problem wie bei user, löschen von book -> id doppelt -> mongodb nicht fröhlich
+                    title:req.body.title,
+                    author: req.body.author,
+                    publisher: req.body.publisher,
+                    isbn: req.body.isbn,
+                    keywords: req.body.keywords.split(', '),
+                    typ: req.body.type,
+                    year: req.body.year,
+                    blurb: req.body.blurb,
+                    small_desc: req.body.small_desc,
+                    img: req.body.img, //TODO safe image external and safe only the path
+                    img_desc: req.body.img_desc,
+                    history: [],
+                    read: req.body.read //todo same as with the image
+                })
+                object.save()
+                    .then(doc => {
+                        console.log(doc)
+                        res.redirect('/admin');
+                    })
+                    .catch(err => {
+                        console.error(err)
+                        res.redirect('/admin');
+                    })
+            })
     }else if(req.session.loggedin){
         res.send('Sie sind kein Administrator und so nicht genehmigt diesen Bereich der Webseite aufzusuchen.')
     }else{
---
+++ public/javascripts/new_object.js
@@ -0,0 +1,15 @@
+const form = {
+    title: document.querySelector("input[name='title']"),
+    author: document.querySelector("input[name='author']"),
+    publisher: document.querySelector("input[name='publisher']"),
+    isbn: document.querySelector("input[name='isbn']"),
+    keywords: document.querySelector("input[name='keywords']"),
+    type: document.querySelector("select[name='type']"),
+    year: document.querySelector("input[name='year']"),
+    blurb: document.querySelector("textarea[name='blurb']"),
+    small_desc: document.querySelector("textarea[name='small_desc']"),
+    img: document.querySelector("input[name='img']"),
+    img_desc: document.querySelector("input[name='img_desc']"),
+    read: document.querySelector("input[name='read']"),
+}
+console.log(form);
