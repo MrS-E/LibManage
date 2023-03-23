98aaf4186739d4936192b82cdea08fe5569d4802
MrS-E <simeon.stix@mx.ch>
2023-03-23 17:02:53 +0100

    update links in views, update in public js still outstanding

--- routes/view/admin.js
+++ routes/view/admin.js
@@ -5,6 +5,7 @@ const admin = require('../../components/view/admin')
 router.get('/', admin.view);
 router.get('/?search', admin.search);
 router.get('/new', admin.new)
-router.get("/edit/:id", admin.edit)
+router.get('/edit/:id', admin.edit)
+router.get('/user') //todo user manage page for admin
 
 module.exports = router;
