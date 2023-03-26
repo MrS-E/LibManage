c240bfdb9aece3c59839c31ce3e8d8c69b165d3f
MrS-E <simeon.stix@mx.ch>
2023-03-26 03:05:30 +0200

    removing careless mistake in object edit (keywords weren't split into array)

--- public/javascript/edit_object.js
+++ public/javascript/edit_object.js
@@ -4,7 +4,9 @@ document.querySelector("button#submit").addEventListener('click', ()=>{
         updated[field.name] = field.value;
     }
     updated[document.querySelector('textarea').name]=document.querySelector('textarea').value
+    updated.keywords = updated.keywords.split(', ')
     console.log(updated)
+
     fetch('/api/admin/'+window.location.href.split('/')[window.location.href.split('/').length-1].split('?')[0], {method:"put", headers: {"Content-Type": "application/json",}, body: JSON.stringify(updated)})
         .then((res)=>res.json())
         .then((data)=>{
