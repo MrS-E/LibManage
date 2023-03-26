eb6e6cea96205a1e04978de9df1d805e5c773302
MrS-E <simeon.stix@mx.ch>
2023-03-26 17:06:36 +0200

    check if user has already lent the book (only for e-objects)

--- components/view/liste.js
+++ components/view/liste.js
@@ -20,11 +20,20 @@ exports.object = function (req, res){
 }
 
 exports.lend = function(req, res){
-    if(req.session.loggedin) { //TODO check is already rented by you
+    if(req.session.loggedin) {
         objects.findOne({_id: req.params.id})
             .then((doc)=>{
                 if(doc.typ.split('-')[0] === 'E'){
-                    res.render('sites/lend', {user: req.session.username, name: req.session.name, book: doc, can_be_rented: true})
+                    let rentable = true
+                    for(let d of doc.history) {
+                        console.log(d)
+                        console.log(req.session.userid)
+                        if(d.user == req.session.userid && d.end===null){
+                            rentable = false
+                            break
+                        }
+                    }
+                    res.render('sites/lend', {user: req.session.username, name: req.session.name, book: doc, can_be_rented: rentable})
                 }else{
                     if(doc.history.length === 0 || doc.history[doc.history.length-1].end){
                         res.render('sites/lend', {user: req.session.username, name: req.session.name, book: doc, can_be_rented: true})
