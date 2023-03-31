ead8eb8032deda62b255a21de98495274a029fba
MrS-E <simeon.stix@gmx.ch>
2023-03-31 23:57:58 +0200

    remove todo

--- routes/api/object.js
+++ routes/api/object.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const object = require('../../components/api/object')
 /* GET users listing. */
-//todo make routing better (remove all suburls use /, /?find,/:id instead)
 router.get('/', object.all);
 //router.get('/detail/:id', object.detail);
 router.post('/lend/:id', object.lend);
