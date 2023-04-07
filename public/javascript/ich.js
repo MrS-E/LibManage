330d7da584685db61de11dffd8623074ca53d7cc
MrS-E <simeon.stix@gmx.ch>
2023-04-08 00:19:41 +0200

    build better solution for ich side selection on small screens

--- public/javascript/ich.js
+++ public/javascript/ich.js
@@ -1,2 +1,4 @@
 document.querySelector("#ich_"+window.location.href.split('/')[window.location.href.split('/').length-1].split('?')[0]).classList.remove("nav-link")
 document.querySelector('#option_select').addEventListener('change',(e)=>{window.location.replace(e.target.value)})
+document.querySelector('#option_select option[selected]').removeAttribute("selected")
+document.querySelector('#op_'+window.location.href.split('/')[window.location.href.split('/').length-1].split('?')[0]).setAttribute("selected", true)
