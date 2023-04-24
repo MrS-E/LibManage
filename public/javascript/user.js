057d79502e5ffb42146657aa2d46c1bfea50b537
MrS-E <simeon.stix@gmx.ch>
2023-04-24 17:07:42 +0200

    finishing searchbar in user management page

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -38,10 +38,12 @@ for(let d of document.querySelectorAll("button[name='update']")) {
 
 for(let d of document.querySelectorAll(".user")){
     user.push({
+        _id: d.querySelector(".email").id,
         salutation: d.querySelector(".salutation").innerText,
         firstName: d.querySelector(".firstName").innerText,
         lastName: d.querySelector(".lastName").innerText,
         role: d.querySelector(".role").value,
+        email: d.querySelector(".email").innerText
     })
 }
 
@@ -53,8 +55,37 @@ document.getElementById('search').addEventListener("keydown", (e)=>{
         search()
     }
 })
-document.getElementById('search_btn').addEventListener('click', search())
+document.getElementById('search_btn').addEventListener('click', search)
 
 function search(){
+    const found = []
+    let out = ""
     const search = document.getElementById('search').value
+    for(let d of user){
+        if(d['firstName'].includes(search) || d['lastName'].includes(search) || d['role'].includes(search)){
+            found.push(d)
+        }
+    }
+    for(let d of found){
+        out += "<div class='row rounded mt-2 border-dark border user' id='"+d._id+"'>" +
+            "<div class='col-md-4 col-sm-6'>" +
+            "<p class='salutation'>"+d.salutation+"</p>" +
+            "<p><span class='firstName'>"+d.firstName+"</span><br><span class='lastName'>"+d.lastName+"</span></p>"+
+            "</div>" +
+            "<div class='col-md-4 col-sm-6 border-start border-dark mt-3 mb-3'>" +
+            "<p class='email'>"+d.email+"</p>" +
+            "<select class='form-control role' id='select_"+d._id+"'>" +
+            "<option value='user' "+(d.role==="user"?"selected":"")+">Nutzer</option>" +
+            "<option value='worker' "+(d.role==="worker"?"selected":"")+">Mitarbeiter</option>" +
+            "<option value='admin' "+(d.role==="admin"?"selected":"")+">Admin</option>" +
+            "</select>" +
+            "</div>" +
+            "<div class='col-md-4 col-sm-12 d-grid gap-2 mt-2 mb-2'>" +
+            "<button class='btn btn-block btn-outline-secondary' id='clean_"+d._id+"' name='clean'>Clean History</button>" +
+            "<button class='btn btn-block btn-secondary' id='delete_"+d._id+"' name='delete'>Löschen</button>" +
+            "<button class='btn btn-block btn-primary' id='update_"+d._id+"' name='update'>Updaten</button>" +
+            "</div>" +
+            "</div>"
+    }
+    document.getElementById('user').innerHTML=out
 }
