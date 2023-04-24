faa3501029ea33fad769c9b63d6c79c67ccecad7
MrS-E <simeon.stix@gmx.ch>
2023-04-24 22:10:37 +0200

    update connection string in config file

--- src/db/config.js
+++ src/db/config.js
@@ -1,4 +1,6 @@
-const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
-const database = 'bibliothek';      // REPLACE WITH YOUR DB NAME
+const server = '127.0.0.1:27017'//'bibliothek.ohk107k.mongodb.net'
+const database = 'bibliothek'
+const username = undefined //'web'
+const password = undefined
 
-exports.mogodbUri = `mongodb://${server}/${database}`
+exports.mongodbUri = username?`mongodb://${username}:${password}@${server}/${database}`:`mongodb://${server}/${database}`
