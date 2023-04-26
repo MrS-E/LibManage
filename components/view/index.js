f3b90788b45f4091473aa4f66043928045332600
MrS-E <simeon.stix@gmx.ch>
2023-04-26 19:35:13 +0200

    could not replicate bug

--- components/view/index.js
+++ components/view/index.js
@@ -16,7 +16,7 @@ exports.login = function (req, res) {
 }
 
 exports.home = async function (req, res){
-    /*function sorter(arr){
+    function sorter(arr){
         const c = {}
         const out = []
         for(let d of arr){
@@ -27,14 +27,14 @@ exports.home = async function (req, res){
             out.push([d, c[d]])
         }
         return out
-    }*/
+    }
 
     if(req.session.loggedin) {
         const book_number = 4
-        //const user = await users.findOne({_id:req.session.userid})
+        const user = await users.findOne({_id:req.session.userid})
         let books = []
         const count = await object.count()
-        //if(user.history.length<=0){
+        if(user.history.length<=0){
             console.log("random")
             for(let d = 0; d<book_number; d++) {
                 const skip = Math.abs(Math.floor(Math.random() * (count - 4)));
@@ -45,7 +45,7 @@ exports.home = async function (req, res){
                     d--;
                 }
             }
-        /*}else{ //fixme search bug
+        }else{
             console.log("console")
             let keywords = []
             for(let d of user.history){
@@ -66,7 +66,7 @@ exports.home = async function (req, res){
                 }
             }
 
-        }*/
+        }
         console.log(books)
         res.render('sites/index', {user: req.session.username, name:req.session.name, books: books})
     }else{+
